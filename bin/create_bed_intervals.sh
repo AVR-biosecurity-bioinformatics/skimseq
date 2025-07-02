638f081aa1c639d1fbb3da80c5e1d9c0d7e3c929
Alexander M Piper (DEECA) <alexander.piper@agriculture.vic.gov.au>
2025-07-02 17:17:23 +1000

    Add option to disable N breakpoints

--- bin/create_bed_intervals.sh
+++ bin/create_bed_intervals.sh
@@ -34,26 +34,31 @@ fi
 
 # TODO: Add any extra filters (coverage, masks, etc) to the exclusion list here
 
-# Detect any strings of N bases in the reference genome to define breakpoints
-java -jar $EBROOTPICARD/picard.jar ScatterIntervalsByNs \
+# Locate appropriate breakpoints in the genome using strings of Ns
+if [ "${3}" -ge 1 ] ; then
+  # Detect any strings of N bases in the reference genome to define breakpoints
+  java -jar $EBROOTPICARD/picard.jar ScatterIntervalsByNs \
       --REFERENCE ${9}\
       --OUTPUT_TYPE BOTH \
       --OUTPUT breakpoints.interval_list \
 	    --MAX_TO_MERGE ${3}
+	    
+  # Convert resulting interval list to bed format
+  java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
+  	--INPUT breakpoints.interval_list \
+  	--OUTPUT tmp.bed
+  	
+  # Subset the breakpoints bed to just the included intervals (this is just the genome bed if not provided)
+  bedtools intersect -wa -a tmp.bed -b included_intervals.bed | cut -f1-4 > breakpoints.bed
+  
+  # Subtract any of the excluded intervals - and subset  to just genotypable intervals (ACGTmers ) 
+  bedtools subtract -a breakpoints.bed -b excluded_intervals.bed | awk '/ACGTmer/' > intervals_filtered.bed
+  
+else
+  # Subtract any of the excluded intervals - and make summary file
+  bedtools subtract -a included_intervals.bed -b excluded_intervals.bed > intervals_filtered.bed
+done
 
-# Convert resulting interval list to bed format
-java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
-	--INPUT breakpoints.interval_list \
-	--OUTPUT tmp.bed
-	
-# Subset the breakpoints bed to just the included intervals (this is just the genome bed if not provided)
-bedtools intersect -wa -a tmp.bed -b included_intervals.bed | cut -f1-4 > breakpoints.bed
-
-# Subtract any of the excluded intervals - and make summary file
-bedtools subtract -a breakpoints.bed -b excluded_intervals.bed > breakpoints_excluded.bed
-
-# Filter intervals bedfile to just genotypable intervals (ACGT bases)
-awk '/ACGTmer/' breakpoints_excluded.bed > intervals_filtered.bed
 
 # SPLIT INTERVALS into even groups
 gatk SplitIntervals \
