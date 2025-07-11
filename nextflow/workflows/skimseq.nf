AVR-biosecurity-bioinformatics/skimseq

commit 47cc058bcd30f510314a55097af8742e577fc376

Masking & genomic interval refactor (#71)
Resolves #69, #14, #73, #70

nextflow/workflows/skimseq.nf
@@ -1,18 +1,16 @@
 
 
 //// import subworkflows
-// include { PROCESS_GENOME                                             } from '../subworkflows/process_genome'
-include { FILTER_VARIANTS                                             } from '../subworkflows/filter_variants'
-include { GATK_GENOTYPING                                             } from '../subworkflows/gatk_genotyping'
+// include { PROCESS_GENOME                                         } from '../subworkflows/process_genome'
+include { FILTER_VARIANTS                                           } from '../subworkflows/filter_variants'
+include { GATK_GENOTYPING                                           } from '../subworkflows/gatk_genotyping'
 include { PROCESS_READS                                             } from '../subworkflows/process_reads'
-include { MITO_GENOTYPING                                             } from '../subworkflows/mito_genotyping'
+include { MITO_GENOTYPING                                           } from '../subworkflows/mito_genotyping'
+include { MASK_GENOME                                               } from '../subworkflows/mask_genome'
 
 //// import modules
 include { INDEX_GENOME                                              } from '../modules/index_genome' 
 include { INDEX_MITO                                                } from '../modules/index_mito'
-include { CREATE_BED_INTERVALS                                              } from '../modules/create_bed_intervals' 
-//include { CREATE_INTERVALS                                              } from '../modules/create_intervals' 
-//include { CONVERT_INTERVALS                                             } from '../modules/convert_intervals' 
 
 // Import dummny file
 ch_dummy_file = file("$baseDir/assets/dummy_file.txt", checkIfExists: true)
@@ -33,7 +31,6 @@ workflow SKIMSEQ {
         println "\n*** ERROR: 'params.samplesheet' must be given ***\n"
     }
     
-
     ch_samplesheet 
         .splitCsv ( by: 1, skip: 1 )
         .map { row -> [ 
@@ -42,16 +39,6 @@ workflow SKIMSEQ {
             file( row[2], checkIfExists: true )     // read2
             ] }
         .set { ch_reads }
-
-    //if ( params.mito_genome ){
-    //    ch_mito = Channel
-    //        .fromPath (
-    //            params.mito_genome, 
-    //            checkIfExists: true
-    //        )
-    //} else {
-    //    ch_mito = Channel.empty()
-    //} 
 
     if ( params.ref_genome ){
         ch_genome = Channel
@@ -62,31 +49,30 @@ workflow SKIMSEQ {
     } else {
         ch_genome = Channel.empty()
     } 
-
-    /*
-    Process nuclear genome and create intervals
-    */
-
-    INDEX_GENOME (
-        ch_genome
-    )
-
-    ch_genome_indexed = INDEX_GENOME.out.fasta_indexed.first()
-    ch_genome_bed = INDEX_GENOME.out.bed.first()
-
-    // Handle empty intervals for interval bed
-    if ( params.interval_bed ){
-        ch_interval_bed = Channel
+    
+    //if ( params.mito_genome ){
+    //    ch_mito = Channel
+    //        .fromPath (
+    //            params.mito_genome, 
+    //            checkIfExists: true
+    //        )
+    //} else {
+    //    ch_mito = Channel.empty()
+    //} 
+    
+    // Handle optional include_bed
+    if ( params.include_bed ){
+        ch_include_bed = Channel
             .fromPath (
-                 params.interval_bed, 
+                 params.include_bed, 
                  checkIfExists: true
              )
     } else {
         // Set to whole genome
-        ch_interval_bed = ch_genome_bed
+        ch_include_bed = ch_genome_bed
     } 
 
-    // Handle empty intervals for exclude bed
+    // Handle optional exclude_bed
     if ( params.exclude_bed ){
         ch_exclude_bed = Channel
             .fromPath (
@@ -97,32 +83,22 @@ workflow SKIMSEQ {
         ch_exclude_bed = ch_dummy_file
     }
     
-    // create groups of genomic intervals for parallel genotyping
-    CREATE_BED_INTERVALS (
-        ch_genome_indexed,
-        params.interval_size,
-        params.interval_n,
-        ch_interval_bed,
-        params.interval_padding,
-        ch_exclude_bed,
-        params.exclude_padding,
-        params.mito_contig
+
+    /*
+    Process nuclear genome
+    */
+
+    INDEX_GENOME (
+        ch_genome
     )
 
-    // create intervals channel, with one interval_list file per element
-    CREATE_BED_INTERVALS.out.interval_list
-        .flatten()
-        // get hash from interval_list name as element to identify intervals
-        .map { interval_list ->
-            def interval_hash = interval_list.getFileName().toString().split("\\.")[0]
-            [ interval_hash, interval_list ] }
-        .set { ch_interval_list }
-        
+    ch_genome_indexed = INDEX_GENOME.out.fasta_indexed.first()
+    ch_genome_bed = INDEX_GENOME.out.genome_bed.first()
+
     /*
     Process mitochondrial genome and create intervals
     */
         
-    // TODO: Extract mitochondrial genome contig from reference contig
     INDEX_MITO (
         ch_genome,
         params.mito_contig
@@ -139,34 +115,65 @@ workflow SKIMSEQ {
         ch_reads,
         ch_genome_indexed
     )
+    
+    PROCESS_READS.out.bam
+      .set{ ch_sample_bam }
 
+    /*
+    Create genomic masks
+    */
+
+      MASK_GENOME(
+        ch_genome_indexed,
+        ch_include_bed,
+        ch_exclude_bed,
+        ch_mito_bed,
+        ch_sample_bam
+      )
+    
     /*
     Call mitochondrial variants and make consensus fasta
     */
 
     MITO_GENOTYPING (
-        PROCESS_READS.out.bam,
+        ch_sample_bam,
         ch_mito_indexed,
         ch_mito_bed
     )
     
     /*
-    Call variants per sample, then combine and joint-genotype across genomic intervals
+    Call muclear variants per sample, then combine and joint-genotype across genomic intervals
     */
 
+    // If mask_before_genotyping is set, use all masks, otherwise just mask mitochondria
+    if ( !params.genotype_masked_bases ){
+            ch_mask_bed_gatk = MASK_GENOME.out.mask_bed
+         } else {
+            ch_mask_bed_gatk = ch_mito_bed
+    }
+    
     GATK_GENOTYPING (
-        PROCESS_READS.out.bam,
+        ch_sample_bam,
         ch_genome_indexed,
-        ch_interval_list
+        ch_include_bed,
+        ch_mask_bed_gatk
     )
 
     /*
     Filter SNP and INDEL variants
     */
 
+    // If mask_before_filtering is set, use all masks, otherwise provide empty dummy file
+    if ( params.filter_masked_variants ){
+          ch_mask_bed_vcf = MASK_GENOME.out.mask_bed
+        } else {
+          ch_mask_bed_vcf = ch_dummy_file
+    }
+    
     FILTER_VARIANTS (
         GATK_GENOTYPING.out.vcf,
-        ch_genome_indexed
+        ch_genome_indexed,
+        ch_mask_bed_vcf
     )
 
 }

bin/call_variants.sh
@@ -8,7 +8,10 @@ set -u
 # $4 = bam file
 # $5 = ref genome
 # $6 = interval hash
-# $7 = interval_list
+# $7 = interval_bed
+# $8 = interval_padding
+# $9 = exclude_bed
+# $10 = exclude_padding
 
 # Create list of bams to be processed
 echo ${4} | tr ' ' '\n' > bam.list
@@ -22,4 +25,8 @@ gatk --java-options "-Xmx${2}G" HaplotypeCaller \
     --native-pair-hmm-threads ${1} \
     --min-base-quality-score 15 \
     --min-pruning 0 \
+    --interval-padding ${8} \
+    --exclude-intervals ${9} \
+    --interval-exclusion-padding ${10} \
+    --interval-merging-rule ALL \
     -ERC GVCF

bin/combine_gvcfs.sh
@@ -19,4 +19,5 @@ gatk --java-options "-Xmx${2}G" CombineGVCFs  \
     -V sample.list \
     -L ${5} \
     --tmp-dir /tmp \
+    --interval-merging-rule ALL \
     -O ${4}.combined.g.vcf.gz

bin/count_reads_bins.sh
@@ -1,0 +1,22 @@
+#!/bin/bash
+set -e
+set -u
+## args are the following:
+# $1 = cpus 
+# $2 = memory
+# $3 = bam file
+# $4 = ref genome
+# $5 = interval_bed
+# $6 = sample
+
+# Create list of bams to be processed
+echo ${3} | tr ' ' '\n' > bam.list
+
+# call variants per sample across all the bam chunks
+gatk --java-options "-Xmx${2}G" CollectReadCounts \
+    -R $4 \
+    -I bam.list \
+    -L $5 \
+    --interval-merging-rule OVERLAPPING_ONLY \
+    --format TSV \
+    -O ${6}.counts.tsv 

bin/create_bed_intervals.sh
@@ -3,79 +3,81 @@ set -e
 set -u
 ## args are the following:
 # $1 = cpus 
-# $2 = interval_size
-# $3 = interval_n
-# $4 = included_bed
-# $5 = included_padding
-# $6 = excluded_bed
-# $7 = excluded_padding
-# $8 = mitochondrial_contig
-# $9 = Reference_genome
+# $2 = interval_n
+# $3 = interval_size
+# $4 = include_bed     
+# $5 = exclude_bed
+# $6 = interval_subdivide_balanced
+# $7 = Reference_genome
 
-cat ${4} > included_intervals.bed
+# parse interval_subdivide options
+if [[ ${6} == "true" ]];   then SUBDIVISION_MODE="--subdivision-mode INTERVAL_SUBDIVISION "; \
+else SUBDIVISION_MODE="--subdivision-mode  BALANCING_WITHOUT_INTERVAL_SUBDIVISION_WITH_OVERFLOW"; fi
 
 # Convert any scientific notation to integers
-interval_size=$(awk -v x="${2}" 'BEGIN {printf("%d\n",x)}')
-interval_n=$(awk -v x="${3}" 'BEGIN {printf("%d\n",x)}')
+interval_n=$(awk -v x="${2}" 'BEGIN {printf("%d\n",x)}')
+interval_size=$(awk -v x="${3}" 'BEGIN {printf("%d\n",x)}')
 
-# Create excluded intervals list if provided, or just an empty dummy file
-if [ -s ${6} ] ; then  
-  bedtools slop -i ${6} -g ${9}.fai -b ${7} > excluded_intervals.bed
-else 
-  touch excluded_intervals.bed
-fi
+# Exclude any intervals if exclusion files are not empty
+# Merge any exclusion masks
+touch merged_masks.bed
+while read mask; do
+  cat $mask | cut -f1-4 >> merged_masks.bed
+done < <(echo ${5} | tr ' ' '\n')
+  
+# Merge any overlapping masks and apply
+bedtools sort -i merged_masks.bed \
+  | bedtools merge -i stdin -c 4 -o distinct \
+  | bedtools subtract -a ${4} -b stdin > included_intervals.bed
 
-# exclude mitochondrial contig if its in the input
-if [ ${8} ] ; then  
-  grep -i ${8} included_intervals.bed >> excluded_intervals.bed
-fi
-
-# Subtract any excluded intervals from included list
-bedtools subtract -a included_intervals.bed -b excluded_intervals.bed > intervals_filtered.bed
- 
-# Create interval groups
+# Calculate number of groups
 if [ "$interval_size" -ge 0 ] && [ "$interval_n" -eq -1 ]; then
     # Only interval_size provided
-    # and then split into approximately $interval_size sized windows
-    bedtools makewindows -b intervals_filtered.bed -w $interval_size > intervals_filtered_windows.bed
-
+    
     # Calculate the total length and number of splits based on interval_size
-    total_length=$(awk -F'\t' 'BEGIN{SUM=0}{ SUM+=$3-$2 }END{print SUM}' intervals_filtered_windows.bed)
+    total_length=$(awk -F'\t' 'BEGIN{SUM=0}{ SUM+=$3-$2 }END{print SUM}' included_intervals.bed)
     n_splits=$(awk -v total_length=$total_length -v interval_size=$interval_size 'BEGIN { print ( total_length / interval_size ) }')
-
-    # Split the BED file into the calculated number of splits
-    bedtools split -i intervals_filtered_windows.bed -n $n_splits -p _split
-
+    
 elif [ "$interval_size" -eq -1 ] && [ "$interval_n" -ge 0 ]; then
     # Only interval_n provided
-    total_length=$(awk '{sum+=$3-$2} END {print sum}' intervals_filtered.bed)
-
-    # Calculate the average window size and split accordingly
-    average_window_size=$((total_length / interval_n))
-    bedtools makewindows -b intervals_filtered.bed -w $average_window_size > intervals_filtered_windows.bed
-    bedtools split -i intervals_filtered_windows.bed -n $interval_n -p _split
-
+    n_splits=$interval_n
+    
 elif [ "$interval_size"  -ge 0 ] && [ "$interval_n" -ge 0 ]; then
-    # Both interval_size and interval_n provided
-    bedtools makewindows -b intervals_filtered.bed -w $interval_size > intervals_filtered_windows.bed
-    bedtools split -i intervals_filtered_windows.bed -n $interval_n -p _split
+    # Both interval_size and interval_n provided, prefer interval_n
+    n_splits=$interval_n
+    
 else
-    # If neither are provided, use the whole inerval
-    cat intervals_filtered.bed > _split.0001.bed
+    # If neither are provided, dont do any splitting
+    n_splits=1
 fi
 
-# Pad and rename each group of output intervals 
-for i in _split.*bed;do
+# Group current intervals into even groups of approximately even base content
+# Optionally subdivide furthr
+gatk SplitIntervals \
+   -R ${7} \
+   -L included_intervals.bed \
+   --scatter-count ${n_splits} \
+   --interval-merging-rule OVERLAPPING_ONLY \
+   -O $(pwd) \
+   $SUBDIVISION_MODE
+   
+# Rename and convert each split output into a bed, and add padding
+for i in *scattered.interval_list;do
   # Hashed output name
-  HASH=$( printf '%s' "$i" | md5sum | awk '{print $1}' ) 
+  HASH=$( md5sum "$i" | awk '{print $1}' ) 
 
-  # Add padding to output
-  bedtools slop -i $i -g ${9}.fai -b ${5} > ${HASH}.bed
-  
-  # remove intermediate interval
-  rm $i
+  # Convert resulting interval list to bed format
+  java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
+  	--INPUT $i \
+  	--OUTPUT tmp.bed
+	
+	#adding extra character at start to ensure that other temp beds dont accidentally get passed to next process
+	cat tmp.bed | cut -f1-4 > _${HASH}.bed
+	
+  # remove intermediate files
+  rm $i tmp.bed
 done
 
-# Remove temporary files
-rm -f included_intervals.bed excluded_intervals.bed intervals_filtered.bed intervals_filtered_windows.bed
+# Remove temporary bed files
+rm -f intervals_filtered.bed included_intervals.bed
 

bin/create_genome_bins.sh
@@ -1,0 +1,30 @@
+#!/bin/bash
+set -e
+set -u
+## args are the following:
+# $1 = cpus 
+# $2 = bin_size
+# $3 = include_bed     
+# $4 = Reference_genome
+
+gatk PreprocessIntervals \
+    -R ${4} \
+    -L ${3} \
+    --bin-length ${2} \
+    --padding 0 \
+    --interval-merging-rule OVERLAPPING_ONLY \
+    -O binned_intervals.interval_list
+
+# Convert resulting interval list to bed format
+java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
+    --INPUT binned_intervals.interval_list \
+  	--OUTPUT tmp.bed
+  	
+cat tmp.bed | cut -f1-3 > binned_intervals.bed
+
+# Annotate intervals
+gatk AnnotateIntervals \
+    -R ${4} \
+    -L binned_intervals.interval_list \
+    --interval-merging-rule OVERLAPPING_ONLY \
+    -O annotated_intervals.tsv

bin/create_genome_masks.sh
@@ -1,0 +1,81 @@
+#!/bin/bash
+set -e
+set -u
+## args are the following:
+# $1 = cpus 
+# $2 = interval_bed
+# $3 = excluded_bed
+# $4 = excluded_padding
+# $5 = use_reference_hardmasks
+# $6 = use_reference_softmasks
+# $7 = Reference_genome
+
+# Included intervals is just the reference genome bed if specific intervals were not provided
+# Just keep the important columns
+cat ${2} | cut -f1-3 > included_intervals.bed
+
+# Create mask bed file
+touch genome_masks.bed
+
+# Add any excluded intervals to hard masked bed if file is not empty
+if [ -s ${3} ] ; then  
+  # Keep just the important columns
+  cat ${3} \
+    | cut -f1-3 \
+    | sed 's/\s*$/\tExcluded/' >> genome_masks.bed
+fi
+
+# Find any N bases in reference genome
+java -jar $EBROOTPICARD/picard.jar ScatterIntervalsByNs \
+      --REFERENCE ${7} \
+      --OUTPUT_TYPE N \
+      --OUTPUT N_bases.interval_list \
+  	  --MAX_TO_MERGE 1
+  	    
+# Convert resulting interval list to bed format 
+java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
+    	--INPUT N_bases.interval_list \
+    	--OUTPUT N_bases.bed
+
+# If use_reference_hardmasks is true, add N base coordinates to the mask file
+if [ ${5} == "true" ] ; then
+  # Subset to just those inside the included intervals and add to masks bed
+  bedtools subtract -a N_bases.bed -b genome_masks.bed \
+    | bedtools intersect -wa -a stdin -b included_intervals.bed \
+    | cut -f1-4 \
+    | sed 's/Nmer/NRef/g' >> genome_masks.bed
+fi
+
+# If exclude_reference_genome_softmasks is true, add any lowercase reference genome bases to soft mask bed
+if [ ${6} == "true" ] ; then
+  
+  # Create temporary reference genome where soft-masked regions are converted to N
+  cat ${7} \
+    | sed '/^[^>]/s/[^ATGC]/N/g' > tmp.fa
+  
+  # Index temporary reference genome and create GATK dictionary
+  samtools faidx tmp.fa
+  gatk CreateSequenceDictionary -R tmp.fa
+
+  # Find all N bases in refernce genome (soft and hard masks now)
+  java -jar $EBROOTPICARD/picard.jar ScatterIntervalsByNs \
+      --REFERENCE tmp.fa \
+      --OUTPUT_TYPE N \
+      --OUTPUT all_masked_bases.interval_list \
+	    --MAX_TO_MERGE 1
+	
+	# Convert resulting interval list to bed format
+	java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
+  	--INPUT all_masked_bases.interval_list \
+  	--OUTPUT all_masked_bases.bed
+  	
+  # Remove temporary fasta
+  rm -f tmp.fa*
+  	
+  # Subtract any intervals that were originally N bases or already contained in masks bed
+  bedtools subtract -a all_masked_bases.bed -b N_bases.bed \
+    | bedtools subtract -a stdin -b genome_masks.bed \
+    | bedtools intersect -wa -a stdin -b included_intervals.bed \
+    | cut -f1-4 \
+    | sed 's/Nmer/SoftMaskRef/g' >> genome_masks.bed
+fi

bin/create_masks_bins.sh
@@ -1,0 +1,75 @@
+#!/bin/bash
+set -e
+set -u
+## args are the following:
+# $1 = cpus 
+# $2 = memory
+# $3 = bin counts files
+# $4 = ref genome
+# $5 = binned_bed
+# $6 = annotated_bins
+# $7 = bin_gc_lower
+# $8 = bin_gc_upper
+# $9 = bin_min_reads
+# $10 = bin_lower_read_perc
+# $11 = bin_upper_read_perc
+# $12 = bin_filter_perc_samples
+
+
+# Create list of bams to be processed
+echo ${3} | tr ' ' '\n' > count.list
+
+# First apply GC percentage filters to bins
+gatk --java-options "-Xmx${2}G" FilterIntervals \
+    -L ${5} \
+    --annotated-intervals ${6} \
+    --interval-merging-rule OVERLAPPING_ONLY \
+    --output gc_filtered.interval_list \
+    --minimum-gc-content ${7} \
+    --maximum-gc-content ${8}
+    
+# Convert resulting interval list to bed format
+java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
+    --INPUT gc_filtered.interval_list \
+  	--OUTPUT gc_passing.bed 
+  	
+# Get the ones that failed the GC filter
+bedtools subtract -a ${5} -b gc_passing.bed \
+  | cut -f1-3 \
+  | sed 's/\s*$/\tGCFilt/' > gc_failed.bed
+
+# Then apply coverage filters to bins
+gatk --java-options "-Xmx${2}G" FilterIntervals \
+    -I count.list \
+    -L ${5} \
+    --interval-merging-rule OVERLAPPING_ONLY \
+    --output cov_filtered.interval_list \
+    --low-count-filter-count-threshold ${9} \
+    --low-count-filter-percentage-of-samples ${12} \
+    --extreme-count-filter-minimum-percentile ${10} \
+    --extreme-count-filter-maximum-percentile ${11} \
+    --extreme-count-filter-percentage-of-samples ${12}
+
+
+# Convert resulting interval list to bed format
+java -jar $EBROOTPICARD/picard.jar IntervalListToBed \
+    --INPUT cov_filtered.interval_list \
+  	--OUTPUT cov_passing.bed 
+  	
+# Get the ones that failed the coverage filter
+bedtools subtract -a ${5} -b cov_passing.bed \
+  | cut -f1-3 \
+  | sed 's/\s*$/\tCovFilt/' > cov_failed.bed
+    
+# Output masks for those which failed both filters
+cat gc_failed.bed > failed.bed
+cat cov_failed.bed >> failed.bed
+bedtools sort -i failed.bed > bin_masked.bed
+
+# Output filter file for those which passed both filters
+bedtools subtract -a ${5} -b bin_masked.bed | cut -f1-4 > bin_filtered.bed
+
+# Filters to add in future:
+# --maximum-mappability
+# --minimum-mappability    
+    

commit 0b2dd9571b587f392e443a673679edfe74da6000

Fixed minor bug introduced in #71 when no intervals were provided

nextflow/workflows/skimseq.nf
@@ -60,6 +60,18 @@ workflow SKIMSEQ {
     //    ch_mito = Channel.empty()
     //} 
     
+    /*
+    Process nuclear genome
+    */
+
+    INDEX_GENOME (
+        ch_genome
+    )
+
+    ch_genome_indexed = INDEX_GENOME.out.fasta_indexed.first()
+    ch_genome_bed = INDEX_GENOME.out.genome_bed
+
+
     // Handle optional include_bed
     if ( params.include_bed ){
         ch_include_bed = Channel
@@ -83,18 +95,6 @@ workflow SKIMSEQ {
         ch_exclude_bed = ch_dummy_file
     }
     
-
-    /*
-    Process nuclear genome
-    */
-
-    INDEX_GENOME (
-        ch_genome
-    )
-
-    ch_genome_indexed = INDEX_GENOME.out.fasta_indexed.first()
-    ch_genome_bed = INDEX_GENOME.out.genome_bed.first()
-
     /*
     Process mitochondrial genome and create intervals
     */