fa5be72fa733e2c99d206659c119eae056338b0c
Alexander M Piper <alexpiperdesigns@gmail.com>
2025-07-30 11:14:58 +1000

    Fixed samtools pipe exit codes resolves #116

--- bin/merge_bam.sh
+++ bin/merge_bam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-set -e
-set -u
+set -uo pipefail   # no -e so we can inspect PIPESTATUS
+
 ## args are the following:
 # $1 = cpus 
 # $2 = sample name
@@ -8,17 +8,30 @@ set -u
 
 ls *.bam > bam.list	
 
-( samtools merge --threads ${1} -O BAM -b bam.list -o - || >&2 echo "samtools merge exit=$?" ) | \
-    ( samtools collate --threads ${1} -O -u - -  || >&2 echo "samtools collate exit=$?"  ) | \
-    ( samtools fixmate --threads ${1} -m - -  || >&2 echo "samtools fixmate exit=$?" ) | \
-    ( samtools sort --threads ${1} -o - - || >&2 echo "samtools sort exit=$?"      ) | \
-    ( samtools markdup --threads ${1} \
+samtools merge --threads ${1} -O BAM -b bam.list -o - \
+    | samtools collate --threads ${1} -O -u - - \
+    | samtools fixmate --threads ${1} -m - - \
+    | samtools sort --threads ${1} -o - - \
+    | samtools markdup --threads ${1} \
         -s -f  ${2}.markdup.json --json \
         -l 300 \
         -d 2500 \
         -S --include-fails \
-        - ${2}.bam \
-    || >&2 echo "samtools markdup exit=$?" )
+        - ${2}.bam 
+
+
+# Capture and report individual tool pipe statuses
+st=("${PIPESTATUS[@]}")
+names=("samtools merge" "samtools collate" "samtools fixmate" "samtools sort" "samtools markdup")
+
+ec=0
+for i in "${!st[@]}"; do
+  if (( st[i] != 0 )); then
+    echo "${names[i]} failed with exit code ${st[i]}" >&2
+    # remember a non-zero to return
+    ec=${st[i]}                  
+  fi
+done
 
 # index bam
 samtools index --threads $1 ${2}.bam
@@ -26,3 +39,6 @@ samtools index --threads $1 ${2}.bam
 # check bam if correctly formatted
 samtools quickcheck ${2}.bam \
 	|| ( echo "BAM file for sample ${2} is not formatted correctly" && exit 1 )
+
+# If any tool returned non-zero, return that exit status to nextflow for retry
+exit "${ec}"           
