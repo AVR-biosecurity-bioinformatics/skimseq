69b44bbf127378a15b64f63664952e50908bb640
Alexander M Piper <alexpiperdesigns@gmail.com>
2025-08-22 17:04:42 +1000

    Add basic fastq checking (#148)
    * Add check that input fastqs are same size i.e. not corrupted #147

--- bin/split_fastq.sh
+++ bin/split_fastq.sh
@@ -11,46 +11,58 @@ set -u
 # Convert any scientific notation to integers
 CHUNK_SIZE=$(awk -v x="${5}" 'BEGIN {printf("%d\n",x)}')
 
-# calculate number of splits based on chunk size
-N_READS=$( seqtk size $3 | cut -f1 )
-
 # Create a file to store intervals
 INTERVALS_FILE="intervals_${2}.csv"
 touch $INTERVALS_FILE  # Create an empty file for intervals
 
-# if N_READS is less than CHUNK_SIZE, don't split file
-if [[ $N_READS -gt $CHUNK_SIZE ]]; then
-    # calculate number of chunks
-    N_CHUNKS=$(( ( $N_READS / $CHUNK_SIZE ) + 1 ))
+# Calculate number of reads in forward and reverse fastqs
+N_READS_F=$( seqtk size $3 | cut -f1 )
+N_READS_R=$( seqtk size $4 | cut -f1 )
 
-    # if number of chunks is larger than number of reads, throw errow
-    if [[ $N_CHUNKS -gt $N_READS || $N_CHUNKS -gt 99999 ]]; then
-        echo "Too many file chunks (${N_CHUNKS}) -- please lower 'params.fastq_chunk_size'"
-        exit 1
-    fi
-   
-    # Calculate the number of reads per chunk (integer division)
-    READS_PER_CHUNK=$((N_READS / N_CHUNKS))
-    
-    # Calculate the remainder (number of reads left to distribute)
-    REMAINING_READS=$((N_READS % N_CHUNKS))
+if [[ "$N_READS_F" == "$N_READS_R" ]]; then
+    # If number of forward and reverse reads match, status = PASS
+    STATUS=PASS
+    N_READS=N_READS_F
 
-    # Return intervals of reads
-    # Loop through each chunk and assign intervals
-    for (( i=1; i<=N_CHUNKS; i++ )); do
-        start=$(( (i - 1) * READS_PER_CHUNK + 1 ))
-        end=$(( i * READS_PER_CHUNK ))
-    
-        # Distribute remaining reads to the last chunk
-        if (( i == N_CHUNKS )); then
-            end=$(( end + REMAINING_READS ))
+    # if N_READS is less than CHUNK_SIZE, don't split file
+    if [[ $N_READS -gt $CHUNK_SIZE ]]; then
+        # calculate number of chunks
+        N_CHUNKS=$(( ( $N_READS / $CHUNK_SIZE ) + 1 ))
+
+        # if number of chunks is larger than number of reads, throw errow
+        if [[ $N_CHUNKS -gt $N_READS || $N_CHUNKS -gt 99999 ]]; then
+            echo "Too many file chunks (${N_CHUNKS}) -- please lower 'params.fastq_chunk_size'"
+            exit 1
         fi
     
-        # Write the interval to the file (in format: sample start end)
-        echo "${start},${end}" >> $INTERVALS_FILE
-    done
+        # Calculate the number of reads per chunk (integer division)
+        READS_PER_CHUNK=$((N_READS / N_CHUNKS))
+        
+        # Calculate the remainder (number of reads left to distribute)
+        REMAINING_READS=$((N_READS % N_CHUNKS))
+
+        # Return intervals of reads
+        # Loop through each chunk and assign intervals
+        for (( i=1; i<=N_CHUNKS; i++ )); do
+            start=$(( (i - 1) * READS_PER_CHUNK + 1 ))
+            end=$(( i * READS_PER_CHUNK ))
+        
+            # Distribute remaining reads to the last chunk
+            if (( i == N_CHUNKS )); then
+                end=$(( end + REMAINING_READS ))
+            fi
+        
+            # Write the interval to the file (in format: sample start end)
+            echo "${start},${end}" >> $INTERVALS_FILE
+        done
+    else
+        # If only one chunk (all reads), print a single line to the file
+        echo "1,${N_READS}" > $INTERVALS_FILE
+    fi
 else
-    # If only one chunk (all reads), print a single line to the file
-    echo "1,${N_READS}" > $INTERVALS_FILE
+    # If number of forward and reverse reads dont match, status = FAIL
+    STATUS=FAIL
 fi
 
+# Output PASS or FAIL for this sample
+echo "${2},${STATUS}" > status.csv
--- nextflow/subworkflows/process_reads.nf
+++ nextflow/subworkflows/process_reads.nf
@@ -51,10 +51,28 @@ workflow PROCESS_READS {
         params.fastq_chunk_size
     )
 
+    // Parse interval CSVs
     SPLIT_FASTQ.out.fastq_interval
         .splitCsv ( by: 1, elem: 3, sep: "," )
 	    .map { sample, read1, read2, intervals -> [ sample, read1, read2, intervals[0], intervals[1] ] }
 	    .set { ch_fastq_split }
+
+
+    // FAIL samples reported by SPLIT_FASTQ
+    SPLIT_FASTQ.out.sample_status
+        .splitCsv ( by: 1, elem: 2, sep: "," )
+        .filter { sample, st -> st == 'FAIL' }
+        .map { sample, st -> sample }
+        .unique()
+        .collect() 
+        .set { ch_fail_samples }
+
+    ch_fail_samples
+    .map { fails ->
+        if (fails && fails.size() > 0)
+            error "Aborting: Fastq(s) are malformed for sample(s): ): ${fails.join(', ')}"
+        true
+    }
 
     /* 
         Read filtering and alignments
