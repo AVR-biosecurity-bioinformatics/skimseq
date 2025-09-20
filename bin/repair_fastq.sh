46ba9c764cc6d30b8b170528ea22171e1fd4804d
Alexander M Piper <alexpiperdesigns@gmail.com>
2025-09-20 11:42:12 +1000

    change repair to seqkit sana

--- bin/repair_fastq.sh
+++ bin/repair_fastq.sh
@@ -8,12 +8,27 @@ set -u
 # $4 = file2
 
 # First ensure all pairs are properly paired - drop those that arent
-repair.sh \
-    in=${3} \
-    in2=${4} \
-    out=${2}_R1.repaired.fastq.gz \
-    out2=${2}_R2.repaired.fastq.gz \
-    tossbrokenreads=t \
-    tossjunk=t \
-    ignorebadquality=t \
-    usejni=t
+#repair.sh \
+#    in=${3} \
+#    in2=${4} \
+#    out=${2}_R1.repaired.fastq.gz \
+#    out2=${2}_R2.repaired.fastq.gz \
+#    tossbrokenreads=t \
+#    tossjunk=t \
+#    ignorebadquality=t \
+#    usejni=t \
+#    cris1Active=false
+
+
+# Alternative with seqkit
+seqkit sana --threads ${1} ${3} -o rescued_1.fq.gz
+seqkit sana --threads ${1} ${4} -o rescued_2.fq.gz
+
+seqkit pair --threads ${1} -1 rescued_1.fq.gz -2 rescued_2.fq.gz -O repaired/
+
+# Copy files out
+mv repaired/rescued_1.fq.gz ${2}_R1.repaired.fastq.gz
+mv repaired/rescued_2.fq.gz ${2}_R2.repaired.fastq.gz
+
+# Clean up
+rm -rf rescued_1.fq.gz rescued_2.fq.gz  repaired
