6985d35b35437b90dc7f1a5af9611c79c604786d
Alexander M Piper <alexpiperdesigns@gmail.com>
2025-09-20 11:59:55 +1000

    Zcat into sana

--- bin/repair_fastq.sh
+++ bin/repair_fastq.sh
@@ -7,26 +7,14 @@ set -u
 # $3 = file1
 # $4 = file2
 
-# First ensure all pairs are properly paired - drop those that arent
-#repair.sh \
-#    in=${3} \
-#    in2=${4} \
-#    out=${2}_R1.repaired.fastq.gz \
-#    out2=${2}_R2.repaired.fastq.gz \
-#    tossbrokenreads=t \
-#    tossjunk=t \
-#    ignorebadquality=t \
-#    usejni=t \
-#    cris1Active=false
+# Sanatise forward and reverse fastq files
+zcat ${3} | seqkit sana --threads ${1} -o rescued_1.fq.gz
+zcat ${4} | seqkit sana --threads ${1} -o rescued_2.fq.gz
 
-
-# Alternative with seqkit
-seqkit sana --threads ${1} ${3} -o rescued_1.fq.gz
-seqkit sana --threads ${1} ${4} -o rescued_2.fq.gz
-
+# Re-pair forward and reverse fastqs
 seqkit pair --threads ${1} -1 rescued_1.fq.gz -2 rescued_2.fq.gz -O repaired/
 
-# Copy files out
+# Rename outputs
 mv repaired/rescued_1.fq.gz ${2}_R1.repaired.fastq.gz
 mv repaired/rescued_2.fq.gz ${2}_R2.repaired.fastq.gz
 
