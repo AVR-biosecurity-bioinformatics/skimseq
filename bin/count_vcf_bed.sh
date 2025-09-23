dcced9b4547366c4a72e3529d5551d7b8c32d332
Alexander M Piper <alexpiperdesigns@gmail.com>
2025-09-23 17:39:22 +1000

    Use sorted algorithm for count_vcf_bed.sh

--- bin/count_vcf_bed.sh
+++ bin/count_vcf_bed.sh
@@ -11,10 +11,16 @@ set -u
 # $7 = sample
 
 # Exclude any intervals in exclude_bed, and ensure they contain only 3 columns
-bedtools subtract -a <(cut -f1-3 "${5}") -b <(cut -f1-3 "${6}") > included_intervals.bed
+# Make sure the bed is sorted in same order as vcf
+bedtools subtract -a <(cut -f1-3 "${5}") -b <(cut -f1-3 "${6}") \
+ | bedtools sort -i stdin -g ${4} > included_intervals.bed
 
 # Count number of VCF records overlapping intervals
+#bcftools query -f '%CHROM\t%POS0\t%POS\n' ${3} \
+#    | bedtools intersect -a included_intervals.bed -b - -c > ${7}.counts.bed
+
 bedtools intersect \
     -a included_intervals.bed \
     -b ${3} \
+    -sorted \
     -c > ${7}.counts.bed
