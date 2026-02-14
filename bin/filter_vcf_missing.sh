ea3a037e10a1e0d09cff61513189f69e73e800ab
Alex Piper <alexpiperdesigns@gmail.com>
2026-02-14 14:56:18 +1100

    Filer_vcf_missing now working

--- bin/filter_vcf_missing.sh
+++ bin/filter_vcf_missing.sh
@@ -13,7 +13,7 @@ set -uoe pipefail
 awk -v thr="$MISSING_FRAC" 'NR==1 {next} $4!="NA" && ($4+0) < thr {print $1}' ${6} > ${4}.${5}.samples.txt
 
 # Drop samples and re-calculate site tags, then drop sites under the missing data filter
-bcftools view -U -S samples_to_keep.txt -Ou ${3} \
+bcftools view -U -S ${4}.${5}.samples.txt -Ou ${3} \
   | bcftools +fill-tags -Ou - -- -t AC,AN,MAF,F_MISSING,NS,'DP:1=int(sum(FORMAT/DP))' \
   | bcftools view -e "INFO/F_MISSING > ${F_MISSING:-1}" -U -Oz9 -o ${4}.${5}.missfiltered.vcf.gz
 
